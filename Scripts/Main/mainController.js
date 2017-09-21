c6fa6b8872ed4a773d1576ad12bcdbdc80e5fd25
mahmah2 <mahfoozi@gmail.com>
2017-09-21 12:09:21 +1000

    Added Owned Houses and Managed Complexes tabs
    owned houses are in a list 
    and managed complexes are in a card each

--- Scripts/Main/mainController.js
+++ Scripts/Main/mainController.js
@@ -3,12 +3,23 @@ angular.module('BM.controllers').
 
 		var vm = this;
 
-		vm.complexes = [
+		vm.ownedHouses = [
+			{ id: 1, logoImage: "img/house.png", title: "House 1", description: "6  James St. Castle Hills 2153" },
+			{ id: 2, logoImage: "img/house.png", title: "House 2", description: "12 Railway St. Castle Hills 2153" },
+			{ id: 3, logoImage: "img/house.png", title: "House 3", description: "17 Pitt St. Castle Hills 2153" },
+			{ id: 4, logoImage: "img/house.png", title: "House 4", description: "54 Philip St. Castle Hills 2153" }
+		];
+
+		vm.managedComplexes = [
 			{id : 1, logoImage : "img/house.png" , title : "Complex 1" , description : "The titles of Washed Out's breakthrough song and the first single from Paracosm share the two most important words in Ernest Greene's musical language: feel it. It's a simple request, as well..."} ,
 			{id : 2, logoImage : "img/house.png" , title : "C2"        , description : "The titles of Washed Out's breakthrough song and the first single from Paracosm share the two most important words in Ernest Greene's musical language: feel it. It's a simple request, as well..."} ,
 			{id : 3, logoImage : "img/house.png" , title : "Complex 3" , description : "The titles of Washed Out's breakthrough song and the first single from Paracosm share the two most important words in Ernest Greene's musical language: feel it. It's a simple request, as well..."} ,
 			{id : 4, logoImage : "img/house.png" , title : "Complex 4" , description : "The titles of Washed Out's breakthrough song and the first single from Paracosm share the two most important words in Ernest Greene's musical language: feel it. It's a simple request, as well..."}
 		];
+
+		vm.houseClick = function (e, house) {
+			alert('house click : ' + house.id);
+		};
 
 		$rootScope.$on('BM.OnSuccessLogin', function () {
 			$state.go('/');
