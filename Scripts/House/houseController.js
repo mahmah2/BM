08cc5614a07ee49019ee7a0ae954422b7b91e8a6
mahmah2 <mahfoozi@gmail.com>
2017-09-22 15:15:27 +1000

    Convert expenses into md-cards
    Convert expenses into md-cards

--- Scripts/House/houseController.js
+++ Scripts/House/houseController.js
@@ -5,10 +5,22 @@ angular.module('BM.controllers').
 		vm.id = $stateParams.id;
 
 		vm.expenses = [
-			{ id: 1, type: "levy"       , amount: "12.50", state : "unpaid", description: "Jan 2017" , dueDate : "24/09/2017"},
-			{ id: 2, type: "landscaping", amount: "22.50", state : "unpaid", description: "Jun 2017" , dueDate : "22/09/2017"},
-			{ id: 3, type: "levy"       , amount: "56.50", state : "paid"  , description: "Sep 2017" , dueDate : "23/09/2017"},
-			{ id: 4, type: "levy"       , amount: "88.50", state : "paid"  , description: "Q4 2017"  , dueDate : "25/10/2017"}
+			{ id: 1, type: "levy"        , amount: "12.50", state : "unpaid", description: "Jan 2017" , dueDate : "24/09/2017"},
+			{ id: 2, type: "landscaping" , amount: "22.50", state : "unpaid", description: "Jun 2017" , dueDate : "22/09/2017"},
+			{ id: 3, type: "water"       , amount: "56.50", state : "paid"  , description: "Sep 2017" , dueDate : "23/09/2017"},
+			{ id: 4, type: "levy"        , amount: "88.50", state : "paid"  , description: "Q4 2017"  , dueDate : "25/10/2017"},
+			{ id: 1, type: "electricity" , amount: "12.50", state : "unpaid", description: "Jan 2017" , dueDate : "24/09/2017"},
+			{ id: 2, type: "landscaping" , amount: "22.50", state : "unpaid", description: "Jun 2017" , dueDate : "22/09/2017"},
+			{ id: 3, type: "levy"        , amount: "56.50", state : "paid"  , description: "Sep 2017" , dueDate : "23/09/2017"},
+			{ id: 4, type: "electricity" , amount: "88.50", state : "paid"  , description: "Q4 2017"  , dueDate : "25/10/2017"},
+			{ id: 1, type: "levy"        , amount: "12.50", state : "unpaid", description: "Jan 2017" , dueDate : "24/09/2017"},
+			{ id: 2, type: "landscaping" , amount: "22.50", state : "unpaid", description: "Jun 2017" , dueDate : "22/09/2017"},
+			{ id: 3, type: "water"       , amount: "56.50", state : "paid"  , description: "Sep 2017" , dueDate : "23/09/2017"},
+			{ id: 4, type: "levy"        , amount: "88.50", state : "paid"  , description: "Q4 2017"  , dueDate : "25/10/2017"},
+			{ id: 1, type: "electricity" , amount: "12.50", state : "unpaid", description: "Jan 2017" , dueDate : "24/09/2017"},
+			{ id: 2, type: "landscaping" , amount: "22.50", state : "unpaid", description: "Jun 2017" , dueDate : "22/09/2017"},
+			{ id: 3, type: "levy"        , amount: "56.50", state : "paid"  , description: "Sep 2017" , dueDate : "23/09/2017"},
+			{ id: 4, type: "electricity" , amount: "88.50", state : "paid"  , description: "Q4 2017"  , dueDate : "25/10/2017"}
 		];
 		
 
